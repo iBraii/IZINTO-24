da28f8d2911554777bfe6d72b552d0941296de41
Sebastian Delgado <sd1198064@gmail.com>
2020-11-09 11:39:43 -0500

    Movimiento y posicion de camara // Sebastian Delgado

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 posicion = transform.position;
+        posicion.x = player.position.x;
+        transform.position = posicion;
+    }
+}
