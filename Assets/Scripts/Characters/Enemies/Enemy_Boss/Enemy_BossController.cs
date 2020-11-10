fa6b60633ebc6a30cb2a3203cad306708a80222f
Albert Sanchez <animazeromax@gmail.com>
2020-11-09 23:58:16 -0500

    Vida del boss // Sanchez-Medina

---
+++ Assets/Scripts/Characters/Enemies/Enemy_Boss/Enemy_BossController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_BossController : MonoBehaviour
+{
+    private Life cmp_life;
+    private Enemy_BossModel cmp_enemy_model;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cmp_life = GetComponent<Life>();
+        cmp_enemy_model = GetComponent<Enemy_BossModel>();
+        cmp_life.life = cmp_enemy_model.bosslife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cmp_enemy_model.bosslife = cmp_life.life;
+
+        Die();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            DamageItself(1);
+        }
+
+    }
+    public void DamageItself(int dmg)
+    {
+        cmp_life.LoseLife(dmg);
+    }
+    void Die()
+    {
+        if (cmp_enemy_model.bosslife <= 0)
+        {
+            Debug.Log("Murió el boss");
+            gameObject.SetActive(false);
+        }
+    }
+}
