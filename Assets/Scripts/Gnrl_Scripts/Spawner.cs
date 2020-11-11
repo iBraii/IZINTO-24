b3114e4fbdc240a0aa3b730a85e6722f3db56033
Sebastian Delgado <sd1198064@gmail.com>
2020-11-10 22:28:49 -0500

    Spawn de rinocerontes, Spawn de enemigos basicos, Spawn de rinoceronte acelerados, Spawn de enemigos basicos acelerados

---
+++ Assets/Scripts/Gnrl_Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float frecuencia;
+    public float retardo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CrearEnemigo", retardo, frecuencia);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CrearEnemigo()
+    {
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+}
