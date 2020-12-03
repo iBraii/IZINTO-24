813927288bbb5faed029dc20d371213115735b5b
Alejandro Medina <almpratto@gmail.com>
2020-12-02 19:45:17 -0500

    Test de juego y correccion de errores 4: Pantalla de derota te lleva al ultimo nivel alcanzado, Spawners de lvl1 reposcicionados //Alejandro Medina - Sebastian Delgado

---
+++ Assets/Scripts/UI&UX/LvlSetValue.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlSetValue : MonoBehaviour
+{
+    public static LvlSetValue lvlSetter;
+    //public static int lvlValue;
+    public int lvlValue;
+    public int lvlNum;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(!lvlSetter)
+        {
+            lvlSetter = this;
+        }
+        else if (lvlSetter != this)
+        {
+            Destroy(lvlSetter.gameObject);
+            lvlSetter = this;
+        }
+    }
+    void Start()
+    {
+
+        lvlValue = lvlNum;
+        Debug.Log(lvlValue);
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI&UX/SceneSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneSelector : MonoBehaviour
+{
+    //public static int lvlValue;
+    // Start is called before the first frame update
+    public GameObject lvlSetObj;
+    public GameObject scnManager;
+    public string[] sceneNames;
+    private int scnNum;
+    void Start()
+    {
+        if (GameObject.Find("LvLStatic"))
+        {
+            lvlSetObj = GameObject.Find("LvLStatic");
+        }
+       if(lvlSetObj == null)
+        {
+            scnNum = 0;
+        }
+        else
+        {
+            
+        scnNum = lvlSetObj.GetComponent<LvlSetValue>().lvlValue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LvlBackScene()
+    {
+        scnManager.GetComponent<SceneChange>().Change(sceneNames[scnNum]);
+    }
+}
